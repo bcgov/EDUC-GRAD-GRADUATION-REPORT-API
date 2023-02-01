7132c356c047672664658787dc391b58257b9846
alexryba <alex.rybakov@gov.bc.ca>
2023-02-01 13:19:04 -0700

    GRAD2-1838 User Request Distribution Run - Certificate OC & RC

--- api/src/test/java/ca/bc/gov/educ/api/grad/report/service/CommonServiceTest.java
+++ api/src/test/java/ca/bc/gov/educ/api/grad/report/service/CommonServiceTest.java
@@ -796,7 +796,7 @@ public class CommonServiceTest {
         Mockito.when(gradStudentCertificatesRepository.findRecordsForUserRequest(studentList)).thenReturn(scdSubList);
 
         List<StudentCredentialDistribution> result = commonService.getStudentCredentialsForUserRequestDisRun(credentialType,new StudentSearchRequest(),null);
-        assertThat(result).isEmpty();
+        assertThat(result).isNotEmpty();
 
     }
 
