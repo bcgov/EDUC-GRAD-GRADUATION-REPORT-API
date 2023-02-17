e9624e69c25803c684fe8a726584bc3b4ec6d1da
alexryba <alex.rybakov@gov.bc.ca>
2023-02-17 10:24:01 -0700

    Removed extra transcript condition

--- api/src/main/java/ca/bc/gov/educ/api/grad/report/repository/GradStudentCertificatesRepository.java
+++ api/src/main/java/ca/bc/gov/educ/api/grad/report/repository/GradStudentCertificatesRepository.java
@@ -35,7 +35,7 @@ public interface GradStudentCertificatesRepository extends JpaRepository<GradStu
 	List<StudentCredentialDistribution> findRecordsForUserRequest(List<UUID> subList);
 
 	@Query(
-		value="select CAST(c.GRADUATION_STUDENT_RECORD_ID AS VARCHAR2(100)) as GRADUATION_STUDENT_RECORD_ID from student_certificate c where c.document_status_code='COMPL' and c.distribution_date is null union select CAST(t.GRADUATION_STUDENT_RECORD_ID AS VARCHAR2(100)) as GRADUATION_STUDENT_RECORD_ID from student_transcript t where ((t.document_status_code='COMPL' and t.distribution_date is null) or (t.document_status_code='COMPL' and t.distribution_date < t.update_date)) and exists ( select 'x' from student_certificate where GRADUATION_STUDENT_RECORD_ID = t.GRADUATION_STUDENT_RECORD_ID and document_status_code='COMPL' and distribution_date is null )", nativeQuery = true
+		value="select CAST(c.GRADUATION_STUDENT_RECORD_ID AS VARCHAR2(100)) as GRADUATION_STUDENT_RECORD_ID from student_certificate c where c.document_status_code='COMPL' and c.distribution_date is null union select CAST(t.GRADUATION_STUDENT_RECORD_ID AS VARCHAR2(100)) as GRADUATION_STUDENT_RECORD_ID from student_transcript t where ((t.document_status_code='COMPL' and t.distribution_date is null) or (t.document_status_code='COMPL' and t.distribution_date < t.update_date))", nativeQuery = true
 	)
 	List<String> findStudentIdForSchoolYearEndReport();
 }
